e7d8437f96c310765d069ff07cf5191f5c6bdff1
rqvl <112122242+rqvl@users.noreply.github.com>
2023-06-30 18:12:40 -0500

    Add files via upload

---
+++ charPicGrid.py
@@ -0,0 +1,23 @@
+def main():
+    grid = [['.', '.', '.', '.', '.', '.'],
+            ['.', 'O', 'O', '.', '.', '.'],
+            ['O', 'O', 'O', 'O', '.', '.'],
+            ['O', 'O', 'O', 'O', 'O', '.'],
+            ['.', 'O', 'O', 'O', 'O', 'O'],
+            ['O', 'O', 'O', 'O', 'O', '.'],
+            ['O', 'O', 'O', 'O', '.', '.'],
+            ['.', 'O', 'O', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.']]
+
+    print(len(grid))
+    print(len(grid[0]))
+
+
+    for y in range(len(grid[0])):
+        for x in range(len(grid)):
+            print(grid[x][y], end='')
+
+        print()
+
+if __name__ == '__main__':
+    main()
