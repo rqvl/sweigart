2d3e5001d8450d87a10637f8960b46698c176397
James <rqvl@runbox.com>
2023-07-02 19:38:59 -0500

    Made small changes

--- ch4-commaCode.py
+++ ch4-commaCode.py
@@ -1,18 +1,29 @@
 # CommaCode.py - from Ch3
 # rqvl@runbox.com
 
-# define a list
+# define lists
 spam = ['apples', 'bananas', 'tofu', 'cats']
+eggs = []
 
 def commaCode(list):
     s = ''
+    
+    #no empty lists
+    if len(list) == 0:
+        s = 'This list is empty.'
+        return s
+    
+    #create string
     for i in range(len(list)-1):
         s += list[i]
         s += ', '
     s += ' and '
     s += list[-1]
-    
     return s
 
+#test cases
 s = commaCode(spam)
+print(s)
+
+s = commaCode(eggs)
 print(s)
