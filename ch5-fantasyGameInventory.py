36e3eb03585335e8ff250a7798a315755eb31888
James <rqvl@runbox.com>
2023-07-04 13:42:41 -0500

    Added list-to-dict function

--- ch5-fantasyGameInventory.py
+++ ch5-fantasyGameInventory.py
@@ -4,6 +4,14 @@
 # James // rqvl@runbox.com
 
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+def addToInventory(inventory, addedItems):
+    for item in addedItems:
+        inventory.setdefault(item,0)
+        inventory[item] += 1
+    return inventory
 
 def displayInventory(inventory):
     print("Inventory:")
@@ -13,4 +21,6 @@ def displayInventory(inventory):
         print(v,k)
     print("Total number of items: " + str(item_total))
 
-displayInventory(stuff)
+inv = addToInventory(inv, dragonLoot)
+
+displayInventory(inv)
