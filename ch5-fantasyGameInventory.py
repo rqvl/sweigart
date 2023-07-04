56ea203306198658e87ede8780b85e5e5c9f174f
James <rqvl@runbox.com>
2023-07-04 18:30:41 -0500

    Minor changes

--- ch5-fantasyGameInventory.py
+++ ch5-fantasyGameInventory.py
@@ -3,11 +3,14 @@
 #
 # James // rqvl@runbox.com
 
+import copy
+
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 inv = {'gold coin': 42, 'rope': 1}
 dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 
-def addToInventory(inventory, addedItems): # to do: should return a value, not change dict?
+def addToInventory(inventory, addedItems):
+    inventory = copy.copy(inventory) 
     for item in addedItems:
         inventory.setdefault(item,0)
         inventory[item] += 1
@@ -21,6 +24,7 @@ def displayInventory(inventory):
         print(v,k)
     print("Total number of items: " + str(item_total))
 
-#displayInventory(stuff) # first part of problem
+displayInventory(stuff) # first part of problem
+print()
 inv = addToInventory(inv, dragonLoot) 
 displayInventory(inv) # list-to-dict
